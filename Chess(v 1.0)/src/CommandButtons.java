d79e113efe1a83caa957117167249a50419f0205
roma0297 <roma_02.97@mail.ru>
2016-06-30 22:34:58 +0300

    Первая версия программы (Chess v 1.0)
    Первая версия - функциональный, но урезанный вариант программы:
    1) Нет возможности масштабировать таблицу
    2) Нет возможности сохранять информацию в файл и извлекать её из него
    3) Не полностью корректная работа с цветов
    4) Отсутствие Label-ов

---
+++ Chess(v 1.0)/src/CommandButtons.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CommandButtons  extends JFrame {
+    public CommandButtons() {
+        super("CommandButtons");
+        setSize(350, 250);
+        setLocation(150, 100);
+        setDefaultCloseOperation( EXIT_ON_CLOSE );
+// создаем панель с табличным расположением для
+// выравнивания размеров кнопок
+        JPanel grid = new JPanel(
+                new GridLayout(1, 2, 5, 0) );
+// добавляем компоненты
+        grid.add( new JButton("OK"));
+        grid.add( new JButton("Отмена"));
+// помещаем полученное в панель с последовательным
+// расположением, выравненным по правому краю
+        JPanel flow = new JPanel(
+                new FlowLayout( FlowLayout.RIGHT ));
+        flow.add(grid);
+// помещаем строку кнопок вниз окна
+        add(flow, BorderLayout.SOUTH );
+// выводим окно на экран
+        setVisible(true);
+    }
+}
